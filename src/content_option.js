7a73e58f752a01bfda4fa5914ce9c8a160ef7631
daviskirober <victorpeace47@gmail.com>
2025-02-18 16:52:46 +0300

    changed

--- src/content_option.js
+++ src/content_option.js
@@ -1,162 +1,203 @@
+import CSK1 from "./assets/CSK design.png";
+import CSK2 from "./assets/csk design 2.png";
+import Design from "./assets/design 2.jpeg";
+import Figma1 from './assets/design2.png';
+import Design2 from './assets/design3.jpeg';
+import Design3 from './assets/design4.jpeg';
+import Design4 from './assets/design5.jpeg';
+import Design5 from './assets/design6.jpeg';
+import Design6 from './assets/design7.jpeg';
+import Design7 from './assets/design8.jpeg';
+import Design8 from './assets/design9.png';
+import Design9 from './assets/design10.png';
+import Design10 from './assets/design11.jpeg';
+import Design11 from './assets/design12.png';
+
 const logotext = "MARENDE";
 const meta = {
-    title: "Brian Marende",
-    description: "I am Brian Marende Figma designer _ Full stack devloper,currently a Student at Kirinyaga University",
+  title: "Brian Marende",
+  description:
+    "I am Brian Marende Figma designer _ Full stack devloper,currently a Student at Kirinyaga University",
 };
 
 const introdata = {
-    title: "I am  Brian Marende",
-    animated: {
-        first: "I love figma designing",
-        second: "I love developing FrontEnds",
-        third: "I am interested in learning Amazon Web Service",
-    },
-    description: "Open communications ,Team work , Respect and Hardwork creates SUCCESS",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I am  Brian Marende",
+  animated: {
+    first: "I love figma designing",
+    second: "I love developing FrontEnds",
+    third: "I am interested in learning Amazon Web Service",
+  },
+  description:
+    "Open communications ,Team work , Respect and Hardwork creates SUCCESS",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "I am a good learner and listener, able to communicate well with a group and on an individual level. As a man of faith, i strive to live a life dedicated to chriatian values.",
+  title: "abit about my self",
+  aboutme:
+    "I am a good learner and listener, able to communicate well with a group and on an individual level. As a man of faith, i strive to live a life dedicated to chriatian values.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "Kutus",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Rongai",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Embu",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "Kutus",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Rongai",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Embu",
+    date: "2019",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    
-    
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    
-        
-    
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
 ];
 
-const services = [{
-        title: "Figma Design",
-        description: "I love designing  using Figma coz i find it interesting navigating through the Figma properties.",
-    },
-    {
-        title: "FrontEnd Developer",
-        description: "i am Fullstack developer based in React and Node js but am interested so much in Developing FrontEnds.",
-    },
-    
-    
-    
+const services = [
+  {
+    title: "Figma Design",
+    description:
+      "I love designing  using Figma coz i find it interesting navigating through the Figma properties.",
+  },
+  {
+    title: "FrontEnd Developer",
+    description:
+      "i am Fullstack developer based in React and Node js but am interested so much in Developing FrontEnds.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: CSK1,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.figma.com/design/aBMbnOn5DaWqbXdgDUW42B/SMADY-TINGS?node-id=1124-244&t=3dSYNfoiq00rIPTD-0",
+  },
+  {
+    img: CSK2,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design8,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.figma.com/design/aBMbnOn5DaWqbXdgDUW42B/SMADY-TINGS?node-id=261-3&p=f&t=3dSYNfoiq00rIPTD-0",
+  },
+  {
+    img: Design9,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.figma.com/design/aBMbnOn5DaWqbXdgDUW42B/SMADY-TINGS?node-id=902-50&p=f&t=3dSYNfoiq00rIPTD-0",
+  },
+  {
+    img: Design,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Figma1,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.figma.com/design/aBMbnOn5DaWqbXdgDUW42B/SMADY-TINGS?node-id=634-3&p=f&t=3dSYNfoiq00rIPTD-0",
+  },
+  {
+    img: Design2,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  {
+    img: Design3,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design4,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design5,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design6,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design7,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design10,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: Design11,
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "otienomarende2002@gmail.com",
-    YOUR_FONE: "0115049827",
-    description: "Minimal supervision , Achieve high standards. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "otienomarende2002@gmail.com",
+  YOUR_FONE: "0115049827",
+  description: "Minimal supervision , Achieve high standards. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com/SMADY254",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/otieno-marende-4b22a8276/",
+  twitter: "https://twitter.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
